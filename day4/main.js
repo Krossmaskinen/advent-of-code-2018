b3ce92140b378e197739593c6415784979af4707
Bill Kurtson <bill.kurtson@gmail.com>
2018-12-04 13:05:45 +0100

    - update day template
    solve day 4

--- day4/main.js
+++ day4/main.js
@@ -7,10 +7,10 @@
         console.log('part1', result1);
         console.timeEnd('part1');
 
-        // console.time('part2');
-        // const result2 = Part2();
-        // console.log('part2', result2);
-        // console.timeEnd('part2');
+        console.time('part2');
+        const result2 = Part2();
+        console.log('part2', result2);
+        console.timeEnd('part2');
     };
 
     main();
